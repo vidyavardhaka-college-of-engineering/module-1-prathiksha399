7ae9bb8ac4c7d378510b41160ed11e47e10a1ce7
prathiksha399 <prathikshaa0803@gmail.com>
2021-03-27 04:08:38 +0000

    done

--- Program-4.c
+++ Program-4.c
@@ -2,17 +2,17 @@
 //OUTPUT: Quotient = 6 Remainder = 1 
 #include <stdio.h>
 int main() {
-    int dividend, divisor, quotient, remainder;
-    //printf("Enter dividend and Divisor ");
-    scanf("%d%d", &dividend,&divisor)  //correct the code
+   int dividend, divisor, quotient, remainder;
+ printf("enter dividend and divisor");
+    scanf("%d%d", &dividend,&divisor); //correct the code
    
     // Computes quotient
-    quotient = dividend  divisor;  //corect the code
+    quotient = dividend / divisor;  //corect the code
 
     // Computes remainder
-    remainder = dividend  divisor; // correct the code
+    remainder = dividend % divisor; // correct the code
 
-    printf("Quotient =  ", quotient);  // correct the code
-    printf("Remainder = ", remainder);  // correct the code
+    printf("Quotient =  %d", quotient);  // correct the code
+    printf("Remainder = %d", remainder);  // correct the code
     return 0;
 }
